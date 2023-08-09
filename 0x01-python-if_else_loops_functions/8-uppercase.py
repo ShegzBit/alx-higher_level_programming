5501762d077fb7eed149f9499368928c2966e36b
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-09 03:16:16 +0100

    Added a possible correction to task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,15 +5,14 @@ def to_upper(c):
         return chr(ord(c) - 32)
     return c
 
+
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
         return True
     return False
 
+
 def uppercase(str):
     for c in str:
         print("{}".format(to_upper(c)), end="")
     print("{}".format("\n"), end="")
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
