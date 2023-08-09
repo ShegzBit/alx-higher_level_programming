ab41a1a20b1dd92a0b3a695cc376d6751833d333
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-09 02:14:49 +0100

    Added a possible correction to task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{0:02d}".format(i))
+    print("{0:02d}".format(i), end="")
+    if i == 99:
+        print("{0}".format("\n"), end="")
+    else:
+        print("{0}".format(", "), end="")
