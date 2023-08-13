0cc249984ec4c64db45fe8d27c0f690e9444b09c
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-13 18:26:41 +0100

    Added a possible solution to task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1:
+        print()
+        exit()
+    for i in matrix:
+        for j in i:
+            if (i.index(j) == len(i) - 1):
+               print("{:d}".format(j))
+            else:
+                print("{:d}".format(j), end=" ")
+
+
+if __name__ == "__main__":
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    print_matrix_integer(matrix)
+    print("--")
+    print_matrix_integer()
