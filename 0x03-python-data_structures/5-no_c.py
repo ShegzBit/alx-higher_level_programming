f6111adcef63a16bb937e9c21e05a0edb8bdf82b
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-13 14:25:07 +0100

    Added a possible solution to task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string is None:
+        return None
+    my_string = "".join([x for x in my_string if(not(x in "cC"))])
+    return my_string
+
+
+if __name__ == "__main__":
+    print(no_c("Best School"))
+    print(no_c("Chicago"))
+    print(no_c("C is fun!"))
+    print(no_c(None))
