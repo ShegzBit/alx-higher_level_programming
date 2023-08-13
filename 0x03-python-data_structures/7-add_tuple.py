80ad4cad0a0b9f2cc6cd07297224b6ad4832a505
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-13 23:51:41 +0100

    Added a solution to task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -20,11 +20,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     return t_3
 
 
-tuple_a = (1, 89)
-tuple_b = (88, 11)
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
 
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
 
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(None, (0, 4, 5, 6)))
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(None, (0, 4, 5, 6)))
