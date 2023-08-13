16dbc6b9222b9ea793b989801ee55a3a0eb4f99e
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-14 00:03:05 +0100

    Solved task 8 -> multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence is None:
+        return (0, None)
+    len_s = len(sentence)
+    f_char = None if len_s == 0 else sentence[0]
+    return len_s, f_char
+
+if __name__ == "__main__":
+    sentence = "At school, I learnt C!"
+    length, first = multiple_returns(None)
+    print("Length: {:d} - First character: {}".format(length, first))
