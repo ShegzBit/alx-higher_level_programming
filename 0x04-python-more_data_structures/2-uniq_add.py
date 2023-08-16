feec68ca3fe92665d29a93a5213e28fa25367ecb
ShegzBit <feranmiawotubo16@gmail.com>
2023-08-17 00:45:06 +0100

    Solved task 2 uniq add

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from functools import reduce
+def uniq_add(my_list=[]):
+    result = reduce((lambda x, y: x + y), set(my_list))
+    return result
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 1, 4, 2, 5]
+    result = uniq_add(my_list)
+    print("Result: {:d}".format(result))
