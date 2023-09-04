a89ca0a9f1562c9e4e0d2c2068228e67ac997c5c
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-04 21:30:52 +0100

    Corrected task 1<2>

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         """Width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -33,7 +33,7 @@ class Rectangle:
         """Height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
