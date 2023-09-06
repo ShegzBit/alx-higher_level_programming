f6bffde7cf193589f7bba5e20dd39f7cd9624523
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-06 15:02:10 +0100

    Added module documentation for locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module for locked class"""
+
 class LockedClass:
+    """Locked class"""
     def __setattr__(self, name, value):
         if name not in ["first_name"]:
             raise AttributeError("""'LockedClass' object \
