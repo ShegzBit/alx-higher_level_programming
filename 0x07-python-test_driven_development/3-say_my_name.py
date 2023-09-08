97e36bd510d9ae65e6d1497a15831ed27ddd7a4f
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-08 07:32:17 +0100

    Solved task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,3 +9,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print(f"My name is {first_name} {last_name}")
+
+
+if __name__ == "__main__":
+    say_my_name()
