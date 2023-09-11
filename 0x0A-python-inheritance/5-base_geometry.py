a3bb103b9a5cb548762650816edf2b154a3f456d
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-11 23:00:45 +0100

    solved task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Empty geometry class"""
+
+
+class BaseGeometry:
+    """Empty Geometry class"""
+    pass
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+
+    print(bg)
+    print(dir(bg))
+    print(dir(BaseGeometry))
