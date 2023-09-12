5bb0686597cf44582a4101061ef3c4e42d6003d2
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-12 01:54:34 +0100

    solved task 12

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for rebel python int class"""
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        """Rebel =="""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Rebel !="""
+        return super().__eq__(other)
+
+
+if __name__ == "__main__":
+    my_i = MyInt(3)
+    print(my_i)
+    print(my_i == 3)
+    print(my_i != 3)
