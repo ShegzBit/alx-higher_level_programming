b25ef0ab0d12b80d08001f3c69885f68d49443e2
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-13 03:45:32 +0100

    solved task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+A Student class module
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of
+        Student
+        """
+        my_dict = {}
+        if attrs is None:
+            my_dict = self.__dict__
+        else:
+            for key in attrs:
+                if key in self.__dict__:
+                    my_dict[key] = self.__dict__.get(key)
+        return my_dict
+
+    def reload_from_json(self, json):
+        """
+        Reloads data froma json into python object
+        """
+        for key, value in json.items():
+            self.key = value
