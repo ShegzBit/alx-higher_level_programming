1ebb5dfd17eaa40856b63be14d9076c053664945
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-13 02:19:23 +0100

    added function documentation

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,5 +7,9 @@ from json import dump
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    creates a json object from a python obj and stores
+    it in filename
+    """
     with open(filename, "w", encoding='utf-8') as f:
         dump(my_obj, f)
