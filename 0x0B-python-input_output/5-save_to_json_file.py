605c715f94ca13ec33aaf80379861bf2abf61b49
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-13 02:17:58 +0100

    solved task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module containing a function that dumps
+the JSON rep of a python object into a file
+"""
+from json import dump
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding='utf-8') as f:
+        dump(my_obj, f)
