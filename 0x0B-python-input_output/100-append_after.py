2b9f41ad827b8b1ba436d2dae15b14a833f75dcf
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-13 03:55:31 +0100

    pushing task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+My module for search and insert function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Appends new_string after search_string
+    """
+
