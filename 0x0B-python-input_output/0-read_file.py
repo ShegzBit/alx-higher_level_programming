8ba4f3a45c37d7faef17ba33f63f96f475a39d54
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-13 01:42:10 +0100

    added more modes <2>

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ def read_file(filename=""):
     """Reads all data in a file"""
     with open(filename, "r",  encoding='utf-8') as f:
         file = f.read()
-    print(file)
+    print(file, end='')
 
 
 if __name__ == "__main__":
