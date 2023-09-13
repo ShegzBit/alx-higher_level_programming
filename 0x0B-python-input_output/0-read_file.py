ba69e9452826f2e994f1d6ba6f2dfb6c6167258d
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-13 01:33:47 +0100

    solved task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module for a read file function"""
+
+
+def read_file(filename=""):
+    """Reads all data in a file"""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
+
+
+if __name__ == "__main__":
+    read_file("my_file_0.txt")
