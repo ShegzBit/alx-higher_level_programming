eff9bbac0a0037493050e2111ff619e938040dc8
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-13 01:35:54 +0100

    added a supposed fix to task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,8 @@
 def read_file(filename=""):
     """Reads all data in a file"""
     with open(filename, encoding='utf-8') as f:
-        print(f.read())
+        file = f.read()
+        print(file)
 
 
 if __name__ == "__main__":
