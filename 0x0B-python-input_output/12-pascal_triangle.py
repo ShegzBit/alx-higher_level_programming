17cf0a449cd25f9d9b7e455c934d3c4f686f7ae7
ShegzBit <feranmiawotubo16@gmail.com>
2023-09-14 01:30:33 +0100

    solved task 12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,4 +9,36 @@ def pascal_triangle(n):
     """
     Pascal triangle
     """
-    return []
+    default = [[1], [1, 1]]
+    if n <= 0:
+        return []
+    if n - 1 < len(default):
+        return default[:n]
+    triangle = default[:]
+    # looping through the outer list
+    for i in range(1, n - 1):
+        j = 0
+        new_row = []
+        new_row.append(1)
+        # looping through each inner list to use value
+        # for generation of new list
+        while j < len(triangle[i]) - 1:
+            prev = triangle[i][j]
+            if j + 1 < len(triangle[i]):
+                # adding current value to next row
+                cur = prev + triangle[i][j + 1]
+            new_row.append(cur)
+            j += 1
+        new_row.append(1)
+        # adding new pascal row to the main triangle
+        triangle.append(new_row)
+    return triangle
+
+
+if __name__ == "__main__":
+    print(pascal_triangle(-2))
+    print(pascal_triangle(0))
+    print(pascal_triangle(1))
+    print(pascal_triangle(2))
+    print(pascal_triangle(3))
+    print(pascal_triangle(4))
