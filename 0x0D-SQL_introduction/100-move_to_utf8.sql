cb89bac533b8c1b55e5e827c6154949bbf96a101
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-18 09:11:46 +0100

    corrected task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,3 @@
 -- my sql task 17
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
