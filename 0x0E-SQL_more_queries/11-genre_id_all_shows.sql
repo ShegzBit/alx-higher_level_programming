1006e4bfbd2a9280a6d52d2c2e97b9fe6049dc48
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 03:45:09 +0100

    UPPERCASED as statement

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,6 @@
 -- tries to use the let out join to get a list of all show
 
 SELECT s.title, g.genre_id
-FROM tv_shows as s LEFT JOIN tv_show_genres as g
+FROM tv_shows AS s LEFT JOIN tv_show_genres AS g
 ON s.id = g.show_id
 ORDER BY s.title, g.genre_id
