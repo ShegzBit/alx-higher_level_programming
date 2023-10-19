a1c59b696663ce1d0874d2b0cd879871c084bb6e
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 21:29:32 +0100

    added backticks for task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- my sql task 17
-ALTER DATABASE hbtn_0c_0
+ALTER DATABASE `hbtn_0c_0`
 CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
@@ -7,12 +7,12 @@ COLLATE = utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 
 -- convert table
-ALTER TABLE first_table
+ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- convert column
-ALTER TABLE first_table
-MODIFY name VARCHAR(256)
+ALTER TABLE `first_table`
+MODIFY `name` VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
