08a661d0da97f7fa387063a04f05aaf03ffb4bdc
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 21:27:18 +0100

    added backticks where needed

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,7 +1,6 @@
 -- 103-max_state.sql
 
-SELECT state, MAX(value) AS max_temp
-FROM temperatures AS t
-WHERE t.month IN (7, 8)
-GROUP BY state
-ORDER BY state;
+SELECT `state`, MAX(value) AS `max_temp`
+FROM `temperatures` AS t
+GROUP BY `state`
+ORDER BY `state`;
