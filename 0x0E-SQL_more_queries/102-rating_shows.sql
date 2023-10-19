073bafd8e6c2b33d537e89c09572d4ece5e4f1ce
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 14:10:37 +0100

    Corrected task 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- a script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 
-SELECT s.title, COUNT(r.rate) AS `rating sum`
+SELECT s.title, SUM(r.rate) AS `rating sum`
 FROM tv_shows AS s INNER JOIN tv_show_ratings AS r
 ON s.id = r.show_id
 GROUP BY s.title
