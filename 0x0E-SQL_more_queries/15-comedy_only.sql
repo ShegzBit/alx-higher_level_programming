38df2d7ee4fc8560173db0c355d118499649025b
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 12:30:54 +0100

    Fixed multiple calls to select

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,8 @@
 --  a script that lists all Comedy shows in the database hbtn_0d_tvshows
 
 SELECT s.title
-FROM tv_shows AS s INNER JOIN tv_show_genres AS g
-ON s.id = g.show_id
-WHERE g.genre_id =
-	(SELECT id
-		FROM tv_genres AS t 
-		WHERE t.name = 'Comedy')
+FROM (tv_shows AS s INNER JOIN tv_show_genres AS g
+ON s.id = g.show_id) INNER JOIN tv_genres AS t
+ON g.genre_id = t.id
+WHERE t.name = 'Comedy'
 ORDER BY title;
