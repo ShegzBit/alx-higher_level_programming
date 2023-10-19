d6c42535d15a4c93fdb58f852a271d7d62e1ae42
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 19:27:03 +0100

    Added a possible solution to task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,17 @@
 -- my sql task 17
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER DATABASE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Select database to use
+USE hbtn_0c_0;
+
+-- convert table
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- convert column
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+COLLATE utf8mb4_unicode_ci;
