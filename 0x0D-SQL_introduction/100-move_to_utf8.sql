75bf7d0a7fea47d16fc06a37f5aad241e7420827
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 19:34:58 +0100

    tried task 17 again

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- my sql task 17
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
 
 -- Select database to use
 USE hbtn_0c_0;
