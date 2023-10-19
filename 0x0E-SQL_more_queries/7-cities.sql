76f47e9cc7a3a14e8a17d9089b33f922ac207783
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 02:19:03 +0100

    corrected task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 
 -- create city if not exists
-CREATE TABLE IF NOT EXISTS city (
+CREATE TABLE IF NOT EXISTS cities (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
 	FOREIGN KEY (state_id) REFERENCES states(id),
