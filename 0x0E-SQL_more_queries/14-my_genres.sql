dd954e67f62a54c47874c93398be5b87ac3d7bdf
ShegzBit <feranmiawotubo16@gmail.com>
2023-10-19 04:30:33 +0100

    Uppercased as statement<2>

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,6 +6,6 @@ FROM tv_genres AS tg INNER JOIN tv_show_genres AS g
 ON tg.id = g.genre_id
 WHERE g.show_id =
 	(SELECT id
-		FROM tv_shows as s
+		FROM tv_shows AS s
 		WHERE s.title = 'Dexter')
 ORDER BY tg.name;
