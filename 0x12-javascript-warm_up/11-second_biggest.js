36606ea30d1bb597963bb1250f6cdee2b58253d5
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-13 19:41:47 +0100

    fixed task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,14 +3,14 @@
 const args = process.argv.slice(2).map(n => Number(n));
 if (args.length <= 1) {
   console.log(0);
-  process.exit(1);
-}
-let biggest = 0;
-let result = 0;
-for (const arg of args) {
-  if (arg > biggest) {
-    result = biggest;
-    biggest = arg;
+} else {
+  let biggest = 0;
+  let result = 0;
+  for (const arg of args) {
+    if (arg > biggest) {
+      result = biggest;
+      biggest = arg;
+    }
   }
+  console.log(result);
 }
-console.log(result);
