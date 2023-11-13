bdfac169ccb7d5ca5216ff4d75036fd51e6c114c
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-13 14:16:44 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-console.log(process.argv);
-console.log(process.argv.length);
 if (process.argv.length <= 2) {
-	console.log("No argument");
+  console.log('No argument');
 } else {
-	console.log("Argument found");
+  console.log('Argument found');
 }
