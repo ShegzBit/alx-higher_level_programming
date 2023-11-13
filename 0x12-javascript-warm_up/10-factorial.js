c813c981744bbf86b1209c8b8cfbdbb283cff318
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-13 17:38:24 +0100

    fixed task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const factorial = (num) => {
+function factorial (num) {
   if (num === 0 || isNaN(num)) { return 1; }
   return num * factorial(num - 1);
-};
+}
 
 const num = Number(process.argv[2]);
 console.log(factorial(num));
