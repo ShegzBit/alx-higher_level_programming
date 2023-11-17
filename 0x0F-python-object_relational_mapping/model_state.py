0a43a70424155884be0c4f340d01c4036b54eb2b
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-17 15:55:01 +0100

    Solved task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+a python file that contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+
+from sqlalchemy.orm import declarative_base as base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import String, Table, Column, Integer, ForeignKey
+import sys
+
+
+Base = base()
+
+
+class State(Base):
+    """
+    the class definition of a State
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
