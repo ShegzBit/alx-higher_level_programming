ba64beec12b10fafcf8bd7120b7ed234d634afc1
ShegzBit <feranmiawotubo16@gmail.com>
2023-11-17 19:39:23 +0100

    solved task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+a script that prints the first State object from
+the database hbtn_0e_6_usa
+"""
+
+from sqlalchemy.orm import declarative_base as base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    _, user, passwd, dbname = sys.argv
+    host = f"mysql://{user}:{passwd}@localhost:3306/{dbname}"
+    engine = create_engine(host)
+    Session = sessionmaker(engine)
+    session = Session()
+
+    print(f"{1}: {session.query(State).order_by(State.id).first().name}")
