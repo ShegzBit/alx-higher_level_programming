33c6fa0e36fcaeaf0aabe81aebbc0d91a2276042
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-01 19:07:09 +0100

    made sure it doesn't run on import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ A script that gets the value of an header
 import urllib.request
 import sys
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(response.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.headers.get('X-Request-Id'))
