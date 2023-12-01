c2e4257a57a64d8d6a42c16442634f3a6ee602f8
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-01 19:33:57 +0100

    solved task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+A python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a
+parameter, and displays the body of the response (decoded in utf-8)
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+url, email = sys.argv[1:]
+payload = {'email': email}
+payload = urllib.parse.urlencode(payload).encode('ascii')
+with urllib.request.urlopen(url, payload) as response:
+    print(response.read().decode('utf-8'))
