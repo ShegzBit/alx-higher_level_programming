34bfc4397ddf00abe7ea22789b56e0b5d30ae0f4
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-01 18:57:14 +0100

    solved task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+A script that gets the value of an header
+"""
+import urllib.request
+import sys
+import socket
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    print(response.headers.get('X-Request-Id'))
