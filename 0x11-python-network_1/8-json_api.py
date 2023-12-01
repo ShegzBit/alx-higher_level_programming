8fd873218c5e786493df86b45a41a299040d2b67
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-01 21:43:28 +0100

    solved task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a letter and sends
+a POST request to
+"""
+import requests as req
+import sys
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    search = sys.argv[1] if len(sys.argv) > 2 else ""
+    payload = {"q": search}
+    res = req.post(url, data=payload)
+    try:
+        my_dict = res.json()
+    except req.json.JSONDecodeError as e:
+        print("Not a valid JSON")
+        exit(0)
+    if not my_dict:
+        print("No result")
+        exit(0)
+    print(f"[{my_dict.get('id')}] my_dict.get('name')")
