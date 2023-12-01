258562121704a8461966d421d33a80a8b1df525c
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-01 05:49:19 +0100

    updated peak.py template

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+A script to find the peak in an unsorted list
+"""
+
+def find_peak(list_of_integers):
+    """
+    method that finds the peak in a unsorted list
+    """
+    return list_of_integers[-2]
