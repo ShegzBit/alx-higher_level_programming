d11b18a554d747f5b9f12a32a374f4f9b46e3b70
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-01 14:56:00 +0100

    solved task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+A script that fetches a url and prints the body
+"""
+import urllib.request
+
+response = urllib.request.urlopen("https://alx-intranet.hbtn.io/status")
+data = response.read()
+print("Body response:")
+print(f'\t- type: {type(data)}')
+print(f'\t- content: {data}')
+print(f'\t- utf8 content: {data.decode("utf-8")}')
