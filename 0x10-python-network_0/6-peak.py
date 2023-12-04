bb9a7de54b26bb88ca49b59771ce722af8696244
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-04 03:35:22 +0100

    find some peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,12 +3,28 @@
 A script to find the peak in an unsorted list
 """
 
-def find_peak(list_of_integers):
+
+def find_peak(integers):
     """
     method that finds the peak in a unsorted list
     """
-    if (len(list_of_integers) == 1):
-        return list_of_integers[1]
-    elif (len(list_of_integers) == 0):
+    if (len(integers) == 1):
+        return integers[0]
+    elif (len(integers) == 0):
         return None
-    return list_of_integers[-2]
+    else:
+        real_value = None
+        fallback = []
+        i = 0
+        while i < len(integers):
+            if i - 1 >= 0 and i + 1 <= len(integers) - 1:
+                if (integers[i] >= integers[i - 1] and
+                        integers[i] >= integers[i + 1]):
+                    real_value = integers[i]
+                    break
+            elif i - 1 < 0:
+                if integers[i] >= integers[i - 1]:
+                    fallback.append(integers[i])
+            i += 1
+
+        return (real_value if real_value else fallback[-1])
