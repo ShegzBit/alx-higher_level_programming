3243fb2d287b6b919a671d0e3c658cd950d72d09
ShegzBit <feranmiawotubo16@gmail.com>
2023-12-05 23:39:48 +0100

    task 6 peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/bash
 """
-A script to find the peak in an unsorted list
+method that finds the in unsorted list
 """
 
 
-def find_peak(itg):
+def find_peak(integers):
     """
-    method that finds the peak in a unsorted list
+    method that finds the in unsorted list
     """
-    if itg:
-        print(itg)
-        if len(itg) == 1:
-            return itg[0]
-        mid = (len(itg)) // 2
-        if itg[mid] < itg[mid - 1]:
-            return find_peak(itg[:mid])
-        elif itg[mid] < itg[mid + 1]:
-            return find_peak(itg[mid + 1:])
-        else:
-            return itg[mid]
+    if (len(integers) == 1):
+        return integers[0]
+    elif (len(integers) == 0):
+        return None
+    else:
+        real_value = None
+        fallback = []
+        i = 0
+        while i < len(integers):
+            if i - 1 >= 0 and i + 1 <= len(integers) - 1:
+                if (integers[i] > integers[i - 1] and
+                        integers[i] > integers[i + 1]):
+                    real_value = integers[i]
+                    break
+            elif i - 1 < 0:
+                if integers[i] >= integers[i - 1]:
+                    fallback.append(integers[i])
+            i += 1
+
+        return (real_value if real_value else fallback[0])
