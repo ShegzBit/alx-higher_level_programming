13ab9c992236dbfbbd12783a79b153d54da17935
ShegzBit <feranmiawotubo16@gmail.com>
2024-01-05 01:19:03 +0100

    corrected task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,24 +2,14 @@
 
 const request = require('request');
 const apiUrl = process.argv[2];
-const len = itr => { return Array.from(itr).length; };
 
 request.get(apiUrl, (err, res, body) => {
   if (err) { console.error(err); return; }
   const users = {};
-  let i = 0;
   JSON.parse(body).forEach(task => {
     const id = task.userId;
-    if (!(id in users)) { users[id] = 0 ; }
+    if (!(id in users)) { users[id] = 0; }
     if (task.completed) { users[id] += 1; }
   });
-	console.log(users)
-  /*process.stdout.write('{');
-  users.forEach((value, key) => {
-    (i === 0)
-      ? console.log(` ${key}: ${value},`)
-      : ((i < len(users.keys()) - 1)
-          ? console.log(`  ${key}: ${value},`)
-          : console.log(`  ${key}: ${value} }`)); i++;
-  };*/
+  console.log(users);
 });
