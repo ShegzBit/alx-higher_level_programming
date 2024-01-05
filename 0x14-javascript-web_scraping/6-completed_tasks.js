6aad3363f784141376ea0ef0831b1f4cc2138ff8
ShegzBit <feranmiawotubo16@gmail.com>
2024-01-05 01:17:50 +0100

    solved task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,19 +6,20 @@ const len = itr => { return Array.from(itr).length; };
 
 request.get(apiUrl, (err, res, body) => {
   if (err) { console.error(err); return; }
-  const users = new Map();
+  const users = {};
   let i = 0;
   JSON.parse(body).forEach(task => {
     const id = task.userId;
-    if (!users.has(id)) { users.set(id, 0); }
-    if (task.completed) { users.set(id, users.get(id) + 1); }
+    if (!(id in users)) { users[id] = 0 ; }
+    if (task.completed) { users[id] += 1; }
   });
-  process.stdout.write('{');
+	console.log(users)
+  /*process.stdout.write('{');
   users.forEach((value, key) => {
     (i === 0)
       ? console.log(` ${key}: ${value},`)
       : ((i < len(users.keys()) - 1)
           ? console.log(`  ${key}: ${value},`)
           : console.log(`  ${key}: ${value} }`)); i++;
-  });
+  };*/
 });
